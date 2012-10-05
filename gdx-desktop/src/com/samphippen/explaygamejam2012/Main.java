66b2d1aa53b2494e3fb14581aac2b732e44908ec
Sam Phippen <samphippen@googlemail.com>
2012-10-05 21:58:12 +0100

    Enable gl2
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- gdx-desktop/src/com/samphippen/explaygamejam2012/Main.java
+++ gdx-desktop/src/com/samphippen/explaygamejam2012/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "gdx";
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
         cfg.width = 800 / 2;
         cfg.height = 1280 / 2;
 
