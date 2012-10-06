7032e302d78a83786efb7be21482be68933fc35d
Phill Tew <peatew@gmail.com>
2012-10-06 15:58:47 +0100

    Clear color
    Signed-off-by: Phill Tew <peatew@gmail.com>

--- gdx/src/com/samphippen/explaygamejam2012/GameHolder.java
+++ gdx/src/com/samphippen/explaygamejam2012/GameHolder.java
@@ -101,7 +101,7 @@ public class GameHolder implements ApplicationListener {
     }
 
     public void draw() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor((1f / 255f) * 155f, (1f / 255f) * 121f, (1f / 255f) * 71f, 1);
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
         Matrix4 traslate = new Matrix4().translate(-getCameraOrigin().x,
