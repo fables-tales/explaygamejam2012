70be59903529a9e5a759f3d5242c61ae193e87ae
Sam Phippen <samphippen@googlemail.com>
2012-10-06 13:34:38 +0100

    Drop the new cog cover in
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- gdx/src/com/samphippen/explaygamejam2012/ResourceManager.java
+++ gdx/src/com/samphippen/explaygamejam2012/ResourceManager.java
@@ -12,7 +12,7 @@ public class ResourceManager {
 	
 	public static void loadResources() {
 		mTextures.put("cogA", new Texture(Gdx.files.internal("circle.png")));
-		mTextures.put("tray", new Texture(Gdx.files.internal("tray.png")));
+		mTextures.put("tray", new Texture(Gdx.files.internal("cogcover.png")));
 		mTextures.put("maskbutton", new Texture(Gdx.files.internal("maskbutton.png")));
 		mTextures.put("p1wins", new Texture(Gdx.files.internal("p1_wins.png")));
 		mTextures.put("p2wins", new Texture(Gdx.files.internal("p2_wins.png")));
