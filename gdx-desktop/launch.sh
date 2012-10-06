d6867ec4cc45d110b7a1d6d3500eab5ea60fd653
Sam Phippen <samphippen@googlemail.com>
2012-10-06 17:03:04 +0100

    Add osx launchers
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ gdx-desktop/launch.sh
@@ -0,0 +1,8 @@
+cd `dirname $0`
+java -cp "../gdx-desktop/bin/:\
+../gdx-desktop/libs/gdx-backend-lwjgl.jar:\
+../gdx-desktop/libs/gdx-backend-lwjgl-natives.jar:\
+../gdx-desktop/libs/gdx-natives.jar:\
+../gdx/bin/:\
+../gdx/libs/gdx.jar" com.samphippen.explaygamejam2012.Main
+
